8a94a77261e704b486e2cb38278b8936f584001b
chrisspurgeon89-spec <chrisspurgeon89@gmail.com>
2026-06-22 11:11:56 +0100

    First commit
    Initial import of framework, automation tests for login page

---
+++ Playwright_LoginDemo_18314/tests/logintests.spec.js
@@ -0,0 +1,86 @@
+import {test, expect} from '@playwright/test'
+
+const username="Admin"
+const password="admin123"
+
+
+test.beforeEach (async ({page}) => {
+
+    // Load the login page
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    await expect.soft(page).toHaveTitle('OrangeHRM')
+    // removed visual validation due to flakiness
+    // await expect.soft(page).toHaveScreenshot()
+
+})
+
+test ('Demo Login Positive Test', async({page}) => {
+    
+    // Populate username and password field with valid credentials, submit
+    await page.locator('[name="username"]').fill(username);
+    await page.locator('[name="password"]').fill(password);
+    await page.getByRole('button', {name:'Login'}).click();
+    //Confirm user taken to homepage/dashboard
+    await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
+    //Logout via dropdown
+    await page.getByRole('listitem').locator('i').click();
+    await page.getByRole('menuitem', { name: 'Logout' }).click();
+
+    // removed visual validation due to flakiness
+    // await expect.soft(page).toHaveScreenshot()
+
+})
+
+test ('Demo Login Negative Test 1', async({page}) => {
+    
+    // Attempt login with with invalid password, submit
+    await page.locator('[name="username"]').fill(username);
+    await page.locator('[name="password"]').fill('negative');
+    await page.getByRole('button', {name:'Login'}).click();
+    // Confirm alert message shows "invalid credentials"
+    await expect(page.getByRole('Alert')).toHaveText('Invalid credentials')
+
+    // removed visual validation due to flakiness
+    // await expect.soft(page).toHaveScreenshot()
+
+
+})
+
+test ('Demo Login Negative Test 2', async({page}) => {
+    
+    // Attempt login with invalid username, submit
+    await page.locator('[name="username"]').fill('negative');
+    await page.locator('[name="password"]').fill(password);
+    await page.getByRole('button', {name:'Login'}).click();
+    // Confirm alert message shows "invalid credentials"
+    await expect(page.getByRole('Alert')).toHaveText('Invalid credentials')
+
+    // removed visual validation due to flakiness
+    // await expect.soft(page).toHaveScreenshot()
+
+})
+
+test ('Demo Login Negative Test 3', async({page}) => {
+    
+    // Attempt login without providing credentials, submit
+    await page.getByRole('button', {name:'Login'}).click();
+    // Confirm username field is shown as mandatory via 'Required' message
+    await expect(page.getByText('Required').first()).toHaveText('Required');
+    // Confirm password field is shown as mandatory via 'Required' message
+    await expect(page.getByText('Required').nth(1)).toHaveText('Required');
+
+    // removed visual validation due to flakiness
+    // await expect.soft(page).toHaveScreenshot()
+
+})
+
+
+
+test.afterEach (async ({page}) => {
+
+    // Confirm on expected page (login) and close down
+    await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await expect.soft(page).toHaveTitle('OrangeHRM')
+    await page.close()
+
+})
